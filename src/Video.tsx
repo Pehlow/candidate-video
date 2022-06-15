e8a21933afc453bd76979c5422edbf87960ad9db
jpi <jpi@atolcd.com>
2022-06-15 09:26:51 +0200

    playing around with modifying hello world

--- src/Video.tsx
+++ src/Video.tsx
@@ -15,37 +15,9 @@ export const RemotionVideo: React.FC = () => {
 				width={1920}
 				height={1080}
 				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
+          titleText: 'Hello World',
+          titleColor: '#ff0000',
 				}}
-			/>
-			<Composition
-				id="Logo"
-				component={Logo}
-				durationInFrames={200}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Title"
-				component={Title}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			<Composition
-				id="Subtitle"
-				component={Subtitle}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
 			/>
 		</>
 	);
